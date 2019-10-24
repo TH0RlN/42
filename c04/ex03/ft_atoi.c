c2c67e576c74bed644abfb4b4f3cca456ab0b67f
Raul Arias perez <rarias-p@c3r9s4.42madrid.com>
2019-10-24 21:09:24 +0200

    Todolo feito

--- c04/ex03/ft_atoi.c
+++ c04/ex03/ft_atoi.c
@@ -1,25 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rarias-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/24 00:42:13 by rarias-p          #+#    #+#             */
-/*   Updated: 2019/10/24 20:31:40 by rarias-p         ###   ########.fr       */
+/*   Created: 2019/10/24 12:07:48 by rarias-p          #+#    #+#             */
+/*   Updated: 2019/10/24 20:38:45 by rarias-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-void	ft_putstr(char *str)
+int	ft_atoi(char *str)
 {
 	int i;
+	int negcheck;
+	int a;
 
 	i = 0;
-	while (str[i] != '\0')
+	a = 0;
+	negcheck = 1;
+	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' ||
+			str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+		i++;
+	while (str[i] == '-' || str[i] == '+')
 	{
-		write(1, &str[i], 1);
+		if (str[i] == '-')
+			negcheck *= -1;
 		i++;
 	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		a = a * 10 + (str[i] - '0');
+		i++;
+	}
+	return (a * negcheck);
 }
