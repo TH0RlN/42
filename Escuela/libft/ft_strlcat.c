012e07498dcd072b25ecbe496f30b8cf98e0c98c
Raul Arias perez <rarias-p@c3r8s2.42madrid.com>
2019-11-10 11:00:38 +0100

    strlcat

---
+++ Escuela/libft/ft_strlcat.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rarias-p <rarias-p@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/10 10:38:32 by rarias-p          #+#    #+#             */
+/*   Updated: 2019/11/10 10:59:18 by rarias-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+	j = 0;
+	while (j < size - 1 && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (j);
+}
