d6b6d5bc0c4ca1fc9a3090948d7d95f3d51a44a0
Raul Arias perez <rarias-p@c3r8s5.42madrid.com>
2019-11-12 12:17:35 +0100

    substr fin

--- Escuela/libft/ft_substr.c
+++ Escuela/libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: rarias-p <rarias-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/11 11:45:47 by rarias-p          #+#    #+#             */
-/*   Updated: 2019/11/12 11:29:51 by rarias-p         ###   ########.fr       */
+/*   Updated: 2019/11/12 12:15:14 by rarias-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,22 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	int		i;
 	int		j;
+	int		i;
 	char	*k;
 	int		real_len;
 
-	while (s[i] != start)
+	while (s[start + real_len] != '\0')
+		real_len++;
+	if (!(k = malloc(real_len * sizeof(char))))
+		return (0);
+	j = start;
+	i = 0;
+	while (s[j] != '\0')
+	{
+		k[i] = s[j];
+		j++;
 		i++;
-	k = malloc(i * sizeof(char));
-	
+	}
+	return (k);
 }
