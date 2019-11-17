4f04bc7153295935013b108c0ff69458bee50cb0
Raul Arias perez <rarias-p@c3r7s4.42madrid.com>
2019-11-17 17:05:19 +0100

    hub

--- Escuela/libft/ft_memcpy.c
+++ Escuela/libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: rarias-p <rarias-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 11:11:26 by rarias-p          #+#    #+#             */
-/*   Updated: 2019/11/17 13:52:22 by rarias-p         ###   ########.fr       */
+/*   Updated: 2019/11/17 17:02:24 by rarias-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 
 	auxdest = (char *)dest;
 	auxsrc = (char *)src;
+	if (auxdest == '\0' && auxsrc == '\0')
+		return (0);
 	i = 0;
 	while (i < n)
 	{
