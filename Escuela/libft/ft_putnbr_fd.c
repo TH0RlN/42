ffd76e399384bb0b6d054e122776a257dcb5f05f
Raul Arias perez <rarias-p@c3r9s5.42madrid.com>
2019-11-20 13:28:11 +0100

    yyvuyvc76td7t

--- Escuela/libft/ft_putnbr_fd.c
+++ Escuela/libft/ft_putnbr_fd.c
@@ -1,19 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rarias-p <rarias-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/18 16:12:38 by rarias-p          #+#    #+#             */
-/*   Updated: 2019/11/20 13:00:58 by rarias-p         ###   ########.fr       */
+/*   Created: 2019/11/20 12:49:15 by rarias-p          #+#    #+#             */
+/*   Updated: 2019/11/20 13:19:49 by rarias-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	main(void)
+void	ft_putnbr_fd(int n, int fd)
 {
-	ft_putnbr_fd(-6355, 1);
-	return (0);
+	long int	m;
+	long int	i;
+	long int	j;
+
+	m = n;
+	i = 0;
+	if (m >= 0)
+		i = m;
+	if (m < 0)
+	{
+		i = m * -1;
+		ft_putchar_fd('-', fd);
+	}
+	if (i >= 10)
+		ft_putnbr_fd(i / 10, fd);
+	j = i % 10 + '0';
+	write(fd, &j, 1);
 }
--- Escuela/libft/libft.h
+++ Escuela/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: rarias-p <rarias-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/13 17:08:02 by rarias-p          #+#    #+#             */
-/*   Updated: 2019/11/20 12:36:07 by rarias-p         ###   ########.fr       */
+/*   Updated: 2019/11/20 12:49:48 by rarias-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,9 @@ int		ft_toupper(int c);
 char	*ft_itoa(int n);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-void ft_putchar_fd(char c, int fd);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #	endif
