cc2bed0b60833923aacbf734040ee392274911d8
Raul Arias perez <rarias-p@c3r7s4.42madrid.com>
2019-12-11 10:37:06 +0100

    hbabd

--- Escuela/get_next_line/get_next_line.c
+++ Escuela/get_next_line/get_next_line.c
@@ -6,18 +6,23 @@
 /*   By: rarias-p <rarias-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/09 09:52:48 by rarias-p          #+#    #+#             */
-/*   Updated: 2019/12/10 16:36:03 by rarias-p         ###   ########.fr       */
+/*   Updated: 2019/12/11 10:31:18 by rarias-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include "../libft/libft.h"
 
 int		get_next_line(int fd, char **line)
 {
 	int test;
-	//int i;
+	char prueba[999];
 	
-	test = read(fd, *line, BUFFER_SIZE);
-	printf("%d      %s\n", test, *line);
-	return (0);
+	//test = read(fd, *line, BUFFER_SIZE);
+	//printf("%s || %d\n", *line, test);
+	line = NULL;
+	test = read(fd, prueba, 1);
+	prueba[ft_strlen(prueba)] = '\0';
+	printf("%s || %d\n", prueba, test);
+	return (test);
 }
--- Escuela/get_next_line/main.c
+++ Escuela/get_next_line/main.c
@@ -6,7 +6,7 @@
 /*   By: rarias-p <rarias-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/09 10:22:15 by rarias-p          #+#    #+#             */
-/*   Updated: 2019/12/10 16:45:12 by rarias-p         ###   ########.fr       */
+/*   Updated: 2019/12/11 10:34:00 by rarias-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,28 +21,20 @@ int main()
 	char			*lineadress[66];
 
 	j = 1;
-	if (!(fd = open("files/alphabet", O_RDONLY)))
+	if (!(fd = open("texto.txt", O_RDONLY)))
 	{
 		printf("\nError in open\n");
 		return (0);
 	}
 	while ((i = get_next_line(fd, &line)) > 0)
 	{
-		printf("|%s\n", line);
+		printf("%s\n", line);
 		lineadress[j - 1] = line;
 		j++;
 	}
-	printf("|%s\n", line);
+	printf("%s\n", line);
 	free(line);
 	close(fd);
-
-	if (i == -1)
-		printf ("\nError in Fonction - Returned -1\n");
-	else if (j == 66)
-		printf("\nRight number of lines\n");
-	else if (j != 66)
-		printf("\nNot Good - Wrong Number Of Lines\n");
 	while (--j > 0)
 		free(lineadress[j - 1]);
-	j = 1;
 }
