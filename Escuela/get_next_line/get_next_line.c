fb0cf8b53376c22a56b25b51bc56ef726b3b63db
Raul Arias perez <rarias-p@c3r8s3.42madrid.com>
2019-12-12 20:31:45 +0100

    cambio nombre debugger

--- Escuela/get_next_line/get_next_line.c
+++ Escuela/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rarias-p <rarias-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/09 09:52:48 by rarias-p          #+#    #+#             */
-/*   Updated: 2019/12/11 17:20:07 by rarias-p         ###   ########.fr       */
+/*   Updated: 2019/12/12 20:29:40 by rarias-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,11 @@
 
 int		get_next_line(int fd, char **line)
 {
-	int test;
-	char buff[30];
-	static char *prueba[999];
-
-	line = NULL;
-	buff[0] = '\0';
-	test = read(fd, buff, 10);
-	buff[test] = '\0';
-	ft_memccpy(buff, buff, '\n', BUFFER_SIZE);
-	if (!*prueba)
-		*prueba = ft_strdup(buff);
-	else
-		*prueba = ft_strjoin(*prueba, buff);
-	printf("%s || %d\n", *prueba, test);
-	return (test);
+	static char buff[BUFFER_SIZE + 1];
+	int			test;
+		
+	*line = NULL;
+	if (!(test = read(fd, buff, BUFFER_SIZE)))
+		return (-1);
+		
 }
