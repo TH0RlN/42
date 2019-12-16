2461e4fb2491505af155fbf8ca5631f68ecfdf1a
Raul Arias perez <rarias-p@c2r11s6.42madrid.com>
2019-12-16 09:50:13 +0100

    cambios en get

--- Escuela/get_next_line/get_next_line.c
+++ Escuela/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rarias-p <rarias-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/09 09:52:48 by rarias-p          #+#    #+#             */
-/*   Updated: 2019/12/13 10:41:28 by rarias-p         ###   ########.fr       */
+/*   Updated: 2019/12/16 09:45:28 by rarias-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,18 @@
 
 int		get_next_line(int fd, char **line)
 {
-	static char buff1[BUFFER_SIZE + 1];
-	char		buff2[BUFFER_SIZE + 1];
+	static char	buff[BUFFER_SIZE + 1];
 	int			test;
-	int			i;
-		
-	*line = NULL;
-	if (!(test = read(fd, buff1, BUFFER_SIZE)))
+	
+	if (buff != NULL)
+		*line = ft_strjoin(*line, buff);
+	if(fd < 0 || !line || !BUFFER_SIZE)
 		return (-1);
-	buff1[BUFFER_SIZE] = '\0';
-	i = 0;
-	while (buff1[i] != '\n' && buff1[i] != '\0')
+	while ((test = read(fd, buff, BUFFER_SIZE)))
 	{
-		buff2[i] = buff1[i];
-		i++;
+		if (ft_strchr(buff, '\n') != NULL)
+			break;
 	}
-	buff2[i] = '\0';
-	if (!*line)
-		*line = ft_strdup(buff2);
-	else
-		*line = ft_strjoin(*line, buff2);	
-	return(test < BUFFER_SIZE ? 0 : 1);
+	*buff = *ft_strchr(buff, '\n');
+	return (0);
 }
