8575f56adfa662822cb6c80601cf6022208e7232
Raul Arias perez <rarias-p@c2r7s6.42madrid.com>
2020-03-03 20:54:08 +0100

    735/745

--- Escuela/printf/get_precision.c
+++ Escuela/printf/get_precision.c
@@ -6,7 +6,7 @@
 /*   By: rarias-p <rarias-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/10 16:52:13 by rarias-p          #+#    #+#             */
-/*   Updated: 2020/02/12 12:19:21 by rarias-p         ###   ########.fr       */
+/*   Updated: 2020/03/03 20:52:59 by rarias-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ void	get_precision(t_rasa *tab)
 	if (tab->flags->astp > 0)
 	{
 		tab->flags->precision = (int)tab->data;
+		if (tab->flags->precision < 0)
+			tab->flags->dot = 0;
 		tab->data = va_arg(tab->list, void *);
 	}
 	else
