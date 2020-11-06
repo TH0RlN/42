a2da4f91e4a09c2e9b4c7c29f37aceab77179c4c
Raul Arias perez <rarias-p@c3r7s3.42madrid.com>
2020-11-06 20:02:21 +0100

    casi funciona

--- Escuela/Server/el_que_puede/src/prueba.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIRECTORIES=$(ls -d 36z*/ ../src)
-
-for d in $DIRECTORIES
-do
-
-	echo "$d"
-done
--- Escuela/Server/el_que_puede/srcs/start.sh
+++ Escuela/Server/el_que_puede/srcs/start.sh
@@ -14,16 +14,34 @@
 mkdir /etc/nginx/ssl
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/localhost.key -out /etc/ssl/certs/localhost.crt -subj "/C=ES/ST=Galicia/L=Vigo/O=42MadridFTef/OU=rarias-p/CN=localhost"
 openssl dhparam -out /etc/ssl/certs/localhost.pem 2048
+
+wget https://files.phpmyadmin.net/phpMyAdmin/4.9.5/phpMyAdmin-4.9.5-all-languages.tar.gz
+tar -xvf phpMyAdmin-4.9.5-all-languages.tar.gz
+rm phpMyAdmin-4.9.5-all-languages.tar.gz
+mv phpMyAdmin-4.9.5-all-languages phpmyadmin
+mv phpmyadmin var/www/html/
+mv srcs/config.inc.php var/www/html/phpmyadmin/
+
+wget https://es.wordpress.org/latest-es_ES.tar.gz
+tar -xvf latest-es_ES.tar.gz
+rm latest-es_ES.tar.gz
+mv wordpress var/www/html/
+mv srcs/wp-config.php var/www/html/wordpress/
+
 service mysql start
 echo "CREATE DATABASE wordpress;" | mysql
 echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" | mysql
 echo "update mysql.user set plugin='mysql_native_password' where user='root';" | mysql
 echo "FLUSH PRIVILEGES;" | mysql
+
 service nginx start
 service php7.3-fpm start
+
 chown -R www-data:www-data /var/www/*
 chmod -R 755 /var/www/*
+
 service mysql restart
 service nginx restart
 service php7.3-fpm restart
+
 bash
