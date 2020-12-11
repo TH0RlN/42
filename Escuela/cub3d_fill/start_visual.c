9bf6e6fd3060bfde434ebceb0f51454bc6a75b29
Raul Arias perez <aprariasperez@gmail.com>
2020-12-11 18:19:23 +0100

    Agora abre unha ventá e apecja correctamente

--- Escuela/cub3d_fill/start_visual.c
+++ Escuela/cub3d_fill/start_visual.c
@@ -6,7 +6,7 @@
 /*   By: rarias-p <rarias-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/09 18:41:52 by rarias-p          #+#    #+#             */
-/*   Updated: 2020/12/09 20:21:01 by rarias-p         ###   ########.fr       */
+/*   Updated: 2020/12/11 17:26:54 by rarias-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,6 @@ void	init_loop(t_data *data)
 	data->mlx_ptr = mlx_init();
 	data->win_ptr = mlx_new_window(data->mlx_ptr, data->resx, data->resy,
 	"CUB3D");
-	mlx_hook(data->win_ptr, 17, 0, salida, (void *)0);
+	mlx_hook(data->win_ptr, 17, 0, salida, (void *)data);
 	mlx_loop(data->mlx_ptr);
 }
